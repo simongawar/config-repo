6353c0adeebfe3745bdb711991518b54e087b1e5
Simon Gawar Dak <gawardak@arizona.edu>
2025-09-28 00:27:40 +0200

    updated license

--- licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -1,9 +1,9 @@
 package com.optimagrowth.license.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import org.springframework.hateoas.RepresentationModel;
 
@@ -12,26 +12,47 @@ import lombok.Setter;
 import lombok.ToString;
 
 
+/**
+ * Represents the License Entity used for persistence with a MySQL database
+ * via Spring Data JPA.
+ *
+ * This entity extends RepresentationModel for HATEOAS support.
+ */
 @Getter @Setter @ToString
 @Entity
 @Table(name="licenses")
 public class License extends RepresentationModel<License> {
 
-	@Id
-	@Column(name = "license_id", nullable = false)
-	private String licenseId;
-	private String description;
-	@Column(name = "organization_id", nullable = false)
-	private String organizationId;
-	@Column(name = "product_name", nullable = false)
-	private String productName;
-	@Column(name = "license_type", nullable = false)
-	private String licenseType;
-	@Column(name="comment")
-	private String comment;
+    @Id
+    @Column(name = "license_id", nullable = false)
+    private String licenseId;
 
-	public License withComment(String comment){
-		this.setComment(comment);
-		return this;
-	}
+    private String description;
+
+    @Column(name = "organization_id", nullable = false)
+    private String organizationId;
+
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Column(name = "license_type", nullable = false)
+    private String licenseType;
+
+    @Column(name="comment")
+    private String comment;
+
+    // Default constructor required by JPA
+    public License() {
+        // Default constructor
+    }
+
+    /**
+     * Helper method to set the comment field for method chaining.
+     * @param comment The comment text.
+     * @return The License object.
+     */
+    public License withComment(String comment){
+        this.setComment(comment);
+        return this;
+    }
 }
