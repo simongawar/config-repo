44764e91eef9323ee05cf030a5b34f8c83ebf9de
Simon Gawar Dak <gawardak@arizona.edu>
2025-09-28 15:59:54 +0200

    update RestErrorList

--- licensing-service/src/main/java/com/optimagrowth/license/model/utils/RestErrorList.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/utils/RestErrorList.java
@@ -1,47 +1,47 @@
 package com.optimagrowth.license.model.utils;
 
 
-import org.springframework.http.HttpStatus;
-
 import java.util.ArrayList;
 import static java.util.Arrays.asList;
+
+import org.springframework.http.HttpStatus;
 
 
 /**
  * Error list
  * @author Simon
- * @version
+ * @version 1.0
  * @since Jun 25, 2018
  */
 public class RestErrorList extends ArrayList<ErrorMessage> {
 
 
-	/** Generated Serial Version*/
-	private static final long serialVersionUID = -721424777198115589L;
-	private HttpStatus status;
+    /** Generated Serial Version*/
+    private static final long serialVersionUID = -721424777198115589L;
+    private HttpStatus status;
 
-	public RestErrorList(HttpStatus status, ErrorMessage... errors) {
-		this(status.value(), errors);
-	}
+    public RestErrorList(HttpStatus status, ErrorMessage... errors) {
+        this(status.value(), errors);
+    }
 
-	public RestErrorList(int status, ErrorMessage... errors) {
-		super();
-		this.status = HttpStatus.valueOf(status);
-		addAll(asList(errors));
-	}
+    public RestErrorList(int status, ErrorMessage... errors) {
+        super();
+        this.status = HttpStatus.valueOf(status);
+        addAll(asList(errors));
+    }
 
-	/**
-	 * @return the status
-	 */
-	public HttpStatus getStatus() {
-		return status;
-	}
+    /**
+     * @return the status
+     */
+    public HttpStatus getStatus() {
+        return status;
+    }
 
-	/**
-	 * @param status the status to set
-	 */
-	public void setStatus(HttpStatus status) {
-		this.status = status;
-	}
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
 
 }
