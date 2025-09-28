9d1a0dddfa7343ef92798d493ed4f89e8f533826
Simon Gawar Dak <gawardak@arizona.edu>
2025-09-28 15:36:00 +0200

    updated serviceconfig file

--- licensing-service/src/main/java/com/optimagrowth/license/config/ServiceConfig.java
+++ licensing-service/src/main/java/com/optimagrowth/license/config/ServiceConfig.java
@@ -6,11 +6,27 @@ import org.springframework.context.annotation.Configuration;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Configuration class designed to fetch the 'example.property'
+ * from Spring Cloud Config Server (or local environment) using 
+ * the modern @ConfigurationProperties approach.
+ */
 @Configuration
 @ConfigurationProperties(prefix = "example")
 @Getter @Setter
-public class ServiceConfig{
+public class ServiceConfig {
 
-  private String property;
-    
+    // This property will be populated from application.properties or the Config Server.
+    // The key expected in the configuration file is 'example.property'.
+    private String property;
+
+    /**
+     * Provides the configured property value, resolving the 'getProperty()'
+     * method call used elsewhere (like in the LicenseService).
+     * Since Lombok is used, this method is generated automatically.
+     * @return The configured string property.
+     */
+    public String getProperty(){
+        return property;
+    }
 }
--- licensing-service/src/main/java/com/optimagrowth/license/model/tempCodeRunnerFile.java
+++
@@ -1,1 +0,0 @@
-package com.optimagrowth.license.model;
